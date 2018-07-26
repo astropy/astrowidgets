4e15f11d086af28546810917cd97751834eb5f40
Matthew Craig <mattwcraig@gmail.com>
2018-07-25 23:44:11 -0400

    Subclass from pywidgets.VBox

--- astrowidgets/examples/core.py
+++ astrowidgets/examples/core.py
@@ -1,3 +1,4 @@
+
 # Use this in the future?!
 #from stginga.jupyterw.widget import ImageWidget
 
@@ -71,7 +72,7 @@ from ginga.web.jupyterw.ImageViewJpw import EnhancedCanvasView
 from ginga.util.wcs import raDegToString, decDegToString
 
 
-class ImageWidget(object):
+class ImageWidget(ipyw.VBox):
     """
     Image widget for Jupyter notebook using Ginga viewer.
 
@@ -88,7 +89,7 @@ class ImageWidget(object):
 
     width, height : int
         Dimension of Jupyter notebook's image widget.
-        
+
     use_opencv : bool
         Let Ginga use ``opencv`` to speed up image transformation;
         e.g., rotation and mosaic. If this is enabled and you
@@ -96,12 +97,12 @@ class ImageWidget(object):
 
     """
     def __init__(self, logger=None, width=500, height=500, use_opencv=True):
-        
+        super().__init__()
         # TODO: Is this the best place for this?
         if use_opencv:
             from ginga import trcalc
             trcalc.use('opencv')
-        
+
         self._viewer = EnhancedCanvasView(logger=logger)
         self._is_marking = False
         self._click_center = False
@@ -121,31 +122,31 @@ class ImageWidget(object):
         # Marker
         self.marker = {'type': 'circle', 'color': 'cyan', 'radius': 20}
         self._marktag = 'marktag'
-        
+
         # coordinates display
         self._jup_coord = ipyw.HTML('Coordinates show up here')
         # This needs ipyevents 0.3.1 to work
         self._viewer.add_callback('cursor-changed', self._mouse_move_cb)
         self._viewer.add_callback('cursor-down', self._mouse_click_cb)
-        
+
         # Define a callback that shows the output of a print
         self.print_out = ipyw.Output()
-        
+
         self._cursor = 'bottom'
-        self._widget = ipyw.VBox([self._jup_img, self._jup_coord])
-        
+        self.children = [self._jup_img, self._jup_coord]
+
     @property
     def logger(self):
         """Logger for this widget."""
         return self._viewer.logger
-        
+
     def _mouse_move_cb(self, viewer, button, data_x, data_y):
         """
         Callback to display position in RA/DEC deg.
         """
         if self.cursor is None:  # no-op
             return
-        
+
         image = viewer.get_image()
         if image is not None:
             ix = int(data_x + 0.5)
@@ -154,7 +155,7 @@ class ImageWidget(object):
                 imval = viewer.get_data(ix, iy)
             except Exception:
                 imval = 'N/A'
-            
+
             # Same as setting pixel_coords_offset=1 in general.cfg
             val = 'X: {:.2f}, Y:{:.2f}'.format(data_x + 1, data_y + 1)
             if image.wcs.wcs is not None:
@@ -167,7 +168,7 @@ class ImageWidget(object):
     def _mouse_click_cb(self, viewer, event, data_x, data_y):
         """
         Callback to handle mouse clicks.
-        """        
+        """
         if self.is_marking:
             objs = []
             try:
@@ -183,22 +184,22 @@ class ImageWidget(object):
             obj = self.marker(x=data_x, y=data_y)
             objs.append(obj)
             self._marktag = viewer.canvas.add(self.dc.CompoundObject(*objs))
-            
+
             with self.print_out:
                 print('Selected {} {}'.format(obj.x, obj.y))
-            
+
         elif self.click_center:
             self.center_on((data_x, data_y))
-            
+
             with self.print_out:
                 print('Centered on X={} Y={}'.format(data_x + 1, data_y + 1))
 
-    def _repr_html_(self):
-        """
-        Show widget in Jupyter notebook.
-        """
-        return display(self._widget)
-    
+#     def _repr_html_(self):
+#         """
+#         Show widget in Jupyter notebook.
+#         """
+#         return display(self._widget)
+
     def load_fits(self, fitsorfn, numhdu=None, memmap=None):
         """
         Load a FITS file into the viewer.
@@ -214,7 +215,7 @@ class ImageWidget(object):
         numhdu : int or `None`
             Extension number of the desired HDU.
             If `None`, it is determined automatically.
-            
+
         memmap : bool or `None`
             Memory mapping.
             If `None, it is determined automatically.
@@ -242,7 +243,7 @@ class ImageWidget(object):
 
         """
         from ginga.util.wcsmod.wcs_astropy import AstropyWCS
-        
+
         image = AstroImage(logger=self.logger)
         image.set_data(nddata.data)
         _wcs = AstropyWCS(self.logger)
@@ -270,18 +271,18 @@ class ImageWidget(object):
     def center_on(self, point, pixel_coords_offset=1):
         """
         Centers the view on a particular point.
-        
+
         Parameters
         ----------
         point : tuple or `~astropy.coordinates.SkyCoord`
             If tuple of ``(X, Y)`` is given, it is assumed
             to be in data coordinates.
-        
+
         pixel_coords_offset : {0, 1}
             Data coordinates provided are n-indexed,
             where n is the given value.
             This is ignored if ``SkyCoord`` is provided.
-        
+
         """
         if isinstance(point, SkyCoord):
             self._viewer.set_pan(point.ra.deg, point.dec.deg, coord='wcs')
@@ -292,23 +293,23 @@ class ImageWidget(object):
         """
         Move the center to a point that is given offset
         away from the current center.
-        
+
         Parameters
         ----------
         dx, dy : float
             Offset value. Unit is assumed based on
             ``skycoord_offset``.
-            
+
         skycoord_offset : bool
             If `True`, offset must be given in degrees.
             Otherwise, they are in pixel values.
-        
+
         """
         if skycoord_offset:
             coord = 'wcs'
         else:
             coord = 'data'
-        
+
         pan_x, pan_y = self._viewer.get_pan(coord=coord)
         self._viewer.set_pan(pan_x + dx, pan_y + dy, coord=coord)
 
@@ -345,7 +346,7 @@ class ImageWidget(object):
         This does not affect :meth:`add_markers`.
         """
         return self._is_marking
-    
+
     @is_marking.setter
     def is_marking(self, val):
         if not isinstance(val, bool):
@@ -368,21 +369,21 @@ class ImageWidget(object):
         self.is_marking = False
         if clear_markers:
             self.reset_markers()
-        
+
     @property
     def marker(self):
         """
         Marker to use.
-        
+
         .. todo:: Add more examples.
-        
+
         Marker can be set as follows::
-            
+
             {'type': 'circle', 'color': 'cyan', 'radius': 20}
-        
+
         """
         return self._marker
-    
+
     @marker.setter
     def marker(self, val):
         marker_type = val.pop('type')
@@ -391,7 +392,7 @@ class ImageWidget(object):
         else:  # TODO: Implement more shapes
             raise NotImplementedError(
                 'Marker type "{}" not supported'.format(marker_type))
-        
+
     def get_markers(self, x_colname='x', y_colname='y',
                     pixel_coords_offset=1,
                     skycoord_colname='coord'):
@@ -404,11 +405,11 @@ class ImageWidget(object):
             Column names for X and Y data coordinates.
             Coordinates retured are 0- or 1-indexed, depending
             on ``pixel_coords_offset``.
-            
+
         pixel_coords_offset : {0, 1}
             Data coordinates returned are n-indexed,
             where n is the given value.
-        
+
         skycoord_colname : str
             Column name for ``SkyCoord``, which contains
             sky coordinates associated with the active image.
@@ -425,7 +426,7 @@ class ImageWidget(object):
         except Exception as e:  # No markers
             self.logger.warning(str(e))
             return
-        
+
         image = self._viewer.get_image()
         xy_col = []
 
@@ -434,7 +435,7 @@ class ImageWidget(object):
         else:
             include_skycoord = True
             radec_col = []
- 
+
         # Extract coordinates from markers
         for obj in c_mark.objects:
             if obj.coord == 'data':
@@ -453,23 +454,23 @@ class ImageWidget(object):
 
         if include_skycoord:
             radec_col = np.asarray(radec_col)  # [[ra0, dec0], [ra1, dec1], ...]
-            
+
             # Fill in X,Y from RA,DEC
             mask = np.isnan(xy_col[:, 0])  # One bool per row
             if np.any(mask):
                 xy_col[mask] = image.wcs.wcspt_to_datapt(radec_col[mask])
-            
+
             # Fill in RA,DEC from X,Y
             mask = np.isnan(radec_col[:, 0])
             if np.any(mask):
                 radec_col[mask] = image.wcs.datapt_to_wcspt(xy_col[mask])
-            
+
             sky_col = SkyCoord(radec_col[:, 0], radec_col[:, 1], unit='deg')
-    
+
         # Convert X,Y from 0-indexed to 1-indexed
         if pixel_coords_offset != 0:
             xy_col += pixel_coords_offset
-    
+
         # Build table
         if include_skycoord:
             markers_table = Table(
@@ -477,9 +478,9 @@ class ImageWidget(object):
                 names=(x_colname, y_colname, skycoord_colname))
         else:
             markers_table = Table(xy_col.T, names=(x_colname, y_colname))
-    
+
         return markers_table
-    
+
     def add_markers(self, table, x_colname='x', y_colname='y',
                     pixel_coords_offset=1,
                     skycoord_colname='coord', use_skycoord=False):
@@ -487,7 +488,7 @@ class ImageWidget(object):
         Creates markers in the image at given points.
 
         .. todo::
-        
+
             Later enhancements to include more columns
             to control size/style/color of marks,
 
@@ -512,7 +513,7 @@ class ImageWidget(object):
         use_skycoord : bool
             If `True`, use ``skycoord_colname`` to mark.
             Otherwise, use ``x_colname`` and ``y_colname``.
-            
+
         """
         # TODO: Resolve https://github.com/ejeschke/ginga/issues/672
         # Extract coordinates from table.
@@ -547,7 +548,7 @@ class ImageWidget(object):
         else:
             objs = c_mark.objects
             self._viewer.canvas.delete_object_by_tag(self._marktag)
-        
+
         # TODO: Test to see if we can mix WCS and data on the same canvas
         objs += [self.marker(x=x, y=y, coord=coord_type)
                  for x, y in zip(coord_x, coord_y)]
@@ -557,17 +558,17 @@ class ImageWidget(object):
     def remove_markers(self, arr):
         """
         Remove some but not all of the markers.
-        
+
         Parameters
         ----------
         arr : ``SkyCoord`` or array-like
             Sky coordinates or 2xN array.
-            
+
         """
         # NOTE: How to match? Use np.isclose?
         #       What if there are 1-to-many matches?
         return NotImplementedError
-        
+
     def reset_markers(self):
         """
         Delete all markers.
@@ -583,14 +584,14 @@ class ImageWidget(object):
         List all available options for image stretching.
         """
         return self._viewer.get_color_algorithms()
-    
+
     @property
     def stretch(self):
         """
         The image stretching algorithm in use.
         """
         return self._viewer.rgbmap.dist
-    
+
     # TODO: Possible to use astropy.visualization directly?
     @stretch.setter
     def stretch(self, val):
@@ -604,8 +605,8 @@ class ImageWidget(object):
         """
         List all available options for image auto-cut.
         """
-        return w._viewer.get_autocut_methods()
-        
+        return self._viewer.get_autocut_methods()
+
     @property
     def cuts(self):
         """
@@ -615,7 +616,7 @@ class ImageWidget(object):
         `autocut_options`.
         """
         return self._viewer.get_cut_levels()
-    
+
     # TODO: Possible to use astropy.visualization directly?
     @cuts.setter
     def cuts(self, val):
@@ -646,7 +647,7 @@ class ImageWidget(object):
         else:
             raise NotImplementedError
         self._cursor = val
-        
+
     @property
     def click_center(self):
         """
@@ -666,7 +667,7 @@ class ImageWidget(object):
         elif self.is_marking and val:
             raise ValueError('Cannot set to True while in marking mode')
         self._click_center = val
-        
+
     # TODO: Awaiting https://github.com/ejeschke/ginga/issues/674
     @property
     def click_drag(self):
@@ -679,7 +680,7 @@ class ImageWidget(object):
         is activated.
         """
         raise NotImplementedError
-        
+
     @property
     def scroll_pan(self):
         """
