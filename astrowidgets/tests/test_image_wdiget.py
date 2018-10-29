675b2f39b313512183e8583e82220d461f218f25
Matt Craig <mattwcraig@gmail.com>
2018-10-29 00:06:41 -0500

    Add regression test for add_markers bug #45

--- astrowidgets/tests/test_image_wdiget.py
+++ astrowidgets/tests/test_image_wdiget.py
@@ -1,3 +1,5 @@
+import numpy as np
+from astropy.table import Table
 from ..core import ImageWidget
 
 
@@ -8,3 +10,18 @@ def test_setting_image_width_height():
     image.image_width = width
     image.image_height = height
     assert image._viewer.get_window_size() == (width, height)
+
+
+def test_add_marker_does_not_modify_input_table():
+    # Regression test for #45
+    # Adding markers should not modify the input data table
+    image = ImageWidget(image_width=300, image_height=300)
+    data = np.random.random([300, 300])
+    image.load_array(data)
+    x = [20, 30, 40]
+    y = [40, 80, 100]
+    # Create two separate tables for comparison after add_markers.
+    orig_table = Table(data=[x, y], names=['x', 'y'])
+    in_table = Table(data=[x, y], names=['x', 'y'])
+    image.add_markers(in_table, pixel_coords_offset=5)
+    assert (in_table == orig_table).all()
