9891e53701142d643276fd3f7cb1b6622fc10bd4
Matthew Craig <mattwcraig@gmail.com>
2019-10-14 14:38:14 -0500

    Use conditional instead of try

--- astrowidgets/core.py
+++ astrowidgets/core.py
@@ -696,9 +696,7 @@ class ImageWidget(ipyw.VBox):
                                                            0)
             # In the event a *single* marker has been added, coord_x and coord_y
             # will be scalars. Make them arrays always.
-            try:
-                len(coord_x)
-            except TypeError:
+            if np.isscalar(coord_x):
                 coord_x = np.array([coord_x])
                 coord_y = np.array([coord_y])
         else:  # Use X,Y
