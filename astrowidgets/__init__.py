c4954c496beec5fc0fcb680683a7a84439161241
P. L. Lim <2090236+pllim@users.noreply.github.com>
2025-03-07 21:33:42 -0500

    Stop using _astropy_init.py

--- astrowidgets/__init__.py
+++ astrowidgets/__init__.py
@@ -1,9 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-
-# Packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *  # noqa
-# ----------------------------------------------------------------------------
+try:
+    from .version import version as __version__
+except ImportError:
+    __version__ = ''
 
 from .ginga import *  # noqa
